93eb58ce2bfbe443875e3fb10b3bb9b0f31d98aa
Tolga Ekmen <tolga.ekmen@gmail.com>
2015-09-17 19:00:33 -0400

    Add watch task

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,21 @@
 var gulp = require('gulp');
 var imagemin = require('gulp-imagemin');
 var changed = require('gulp-changed');
+var del = require('del');
 
 gulp.task('minimizeImages', function() {
   return gulp.src('img/*.gif')
     .pipe(changed('build'))
     .pipe(imagemin())
     .pipe(gulp.dest('build'));
+});
+
+gulp.task('clean', function(callback) {
+  del('build/*', callback);
+});
+
+gulp.task('watch', function() {
+  gulp.watch('app/**/*', ['minimizeImages']);
 });
 
 gulp.task('default', ['minimizeImages']);
