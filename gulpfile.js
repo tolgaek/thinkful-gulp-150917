4d65fda116a1df6072652e83d62cf647375de87b
Tolga Ekmen <tolga.ekmen@gmail.com>
2015-09-17 18:35:30 -0400

    Add gulp-changed to improve build performance in minimizing images

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,13 @@
 
 var gulp = require('gulp');
 var imagemin = require('gulp-imagemin');
-
+var changed = require('gulp-changed');
 
 gulp.task('minimizeImages', function() {
   return gulp.src('img/*.gif')
+    .pipe(changed('build'))
     .pipe(imagemin())
     .pipe(gulp.dest('build'));
 });
+
+gulp.task('default', ['minimizeImages']);
